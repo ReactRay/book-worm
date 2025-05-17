56089e52c916f95dc7ea13564cb9fa7f9ea5da6f
Radwan Mansur <raydiaz1899@gmail.com>
2025-05-17 17:17:17 +0300

    fetch books but with pagination

--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -38,4 +38,30 @@ router.post('/', protectRoute, async (req, res) => {
   }
 })
 
+//pagination => infinite scroll
+router.get('/', protectRoute, async (req, res) => {
+  try {
+    const page = req.query.page || 1
+    const limit = req.query.limit || 5
+    const skip = (page - 1) * limit
+
+    const books = await Book.find()
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit)
+      .populate('user', 'username profileImage')
+const totalBooks =  await Book.countDocuments()
+      
+    res.send({
+        books,
+        currentPage: page,
+        totalBooks,
+        totalPages:  Math.ceil(totalBooks / limit);
+    })
+  } catch (error) {
+    console.log('error in fetching books', error)
+    res.status(500).json({ message: 'something went wrong' })
+  }
+})
+
 export default router
