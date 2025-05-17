2f4d864abbed1b6dde3d3c92fabaa51675e2971f
Radwan Mansur <raydiaz1899@gmail.com>
2025-05-17 17:28:41 +0300

    api ready

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,10 +3,13 @@ import dotenv from 'dotenv'
 import authRoutes from './routes/authRoutes.js'
 import bookRoutes from './routes/bookRoutes.js'
 import { connectDB } from './lib/db.js'
+import cors from 'cors'
+
 dotenv.config()
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 const PORT = process.env.PORT || 3000
 
--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -50,13 +50,13 @@ router.get('/', protectRoute, async (req, res) => {
       .skip(skip)
       .limit(limit)
       .populate('user', 'username profileImage')
-const totalBooks =  await Book.countDocuments()
-      
+    const totalBooks = await Book.countDocuments()
+
     res.send({
-        books,
-        currentPage: page,
-        totalBooks,
-        totalPages:  Math.ceil(totalBooks / limit);
+      books,
+      currentPage: page,
+      totalBooks,
+      totalPages: Math.ceil(totalBooks / limit),
     })
   } catch (error) {
     console.log('error in fetching books', error)
@@ -64,50 +64,46 @@ const totalBooks =  await Book.countDocuments()
   }
 })
 
-
 router.delete('/:id', protectRoute, async (req, res) => {
   try {
     const { id } = req.params
-    
+
     const book = await Book.findById(id)
-    
+
     if (!book) {
       return res.status(404).json({ message: 'Book not found' })
     }
-    
+
     if (book.user.toString() !== req.user._id.toString()) {
       return res.status(401).json({ message: 'Unauthorized' })
     }
-    
-    if(book.image && book.image.includes('cloudinary')){
-       try {
+
+    if (book.image && book.image.includes('cloudinary')) {
+      try {
         await cloudinary.uploader.destroy(book.image.split('/').pop())
-       } catch (error) {
+      } catch (error) {
         console.log(error, 'error in deleting image from cloudinary')
-       }
+      }
     }
-    
-    
+
     await book.deleteOne()
-    
+
     res.status(200).json({ message: 'Book deleted successfully' })
-  
   } catch (error) {
     console.log(error, 'error in deleting book')
     res.status(500).json({ message: 'something went wrong' })
   }
 })
 
-
-router.get('/user' , async (req,res) => {
-    
-    
-    try {
-        const books = await Book.find({user: req.user._id}).sort({createdAt: -1})
-        res.send(books)
-    } catch (error) {
-        console.log(error, 'error in fetching user books')
-        res.status(500).json({ message: 'server error' })
-    }
+router.get('/user', async (req, res) => {
+  try {
+    const books = await Book.find({ user: req.user._id }).sort({
+      createdAt: -1,
+    })
+    res.send(books)
+  } catch (error) {
+    console.log(error, 'error in fetching user books')
+    res.status(500).json({ message: 'server error' })
+  }
 })
 export default router
